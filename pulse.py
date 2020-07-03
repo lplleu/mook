3539a704f47a6808475711d13b7c21e50c7cf2c1
LPLLEU <38284033+lplleu@users.noreply.github.com>
2020-07-03 13:08:51 +0200

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -58,7 +58,7 @@ TEMPLATES = [
   {
    'BACKEND':'django.template.backends.django.DjangoTemplates',
    'DIRS':[os.path.join(SETTINGS_PATH, 'templates'),
-           BASE_DIR, 'templates'),
+           os.path.join(BASE_DIR, 'templates'),
            os.path.join(BASE_DIR, 'pulse', 'templates', 'pulse'),],
    'APP_DIRS':True,
    'OPTIONS':{
