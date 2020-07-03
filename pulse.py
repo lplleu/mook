1cd3fc1be781cc0c414f2ef9b3ee30d1ca6c4889
LPLLEU <38284033+lplleu@users.noreply.github.com>
2020-07-03 22:14:36 +0200

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -138,7 +138,7 @@ def zero(request):
 def one(request):
        #return HttpResponse(SETTINGS_PATH)
        today = datetime.datetime.now().date()
-       return render(request, 'pulse.htm', {'today' : today})
+       return render(request, 'pulse.htm', {"today" : today})
        #return HttpResponse(os.path.join(SETTINGS_PATH, 'templates'))
 
 '''
