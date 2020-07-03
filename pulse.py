8154efa7a0c6bc3987db695f144a697ef49f5bb4
LPLLEU <38284033+lplleu@users.noreply.github.com>
2020-07-03 14:51:59 +0200

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'deamaun.herokuapp.com').split(',')
 
 INSTALLED_APPS = [
- 'pulse.apps.AppNameConfig',
+ 'pulse.auth',
  'django.contrib.admin',
  'django.contrib.auth',
  'django.contrib.contenttypes',
