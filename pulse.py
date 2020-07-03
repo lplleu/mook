68efb84251e9d509bcad9d81dc3e2a52d35b8919
LPLLEU <38284033+lplleu@users.noreply.github.com>
2020-07-03 21:24:23 +0200

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -117,7 +117,6 @@ settings.configure(
         os.path.join(BASE_DIR, 'static'),
    ),
    STATIC_URL='/static/',
-   ),
 )
 
 from django.conf.urls import url
@@ -190,7 +189,7 @@ urlpatterns = (
     url('seven', seven),
 )
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
