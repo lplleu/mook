dae088146ddc3d8da31d50056c336135dfcb0e28
LPLLEU <38284033+lplleu@users.noreply.github.com>
2020-07-05 09:43:41 +0200

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -157,7 +157,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 path = '.'
 
 class LandingView(TemplateView):
-  template_name = "deamaun/pulse.htm"
+  template_name = "app/pulse.htm"
   
 def index(request):
     files = os.listdir(path)
