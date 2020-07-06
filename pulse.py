085ee29391dab716cb7c7faf1b39b28e51e914ce
LPLLEU <38284033+lplleu@users.noreply.github.com>
2020-07-06 15:30:32 +0200

    Update pulse.py

--- pulse.py
+++ pulse.py
@@ -327,7 +327,7 @@ def seven(request):
 '''
 
 urlpatterns = patterns(
-    url(r'^$', indecs, {'template' : 'pulse.htm'}),
+    (r'^$', indecs, {'template' : 'pulse.htm'}),
     url(r'^$', index, name='homepage'),
     #url(r'^admin/', admin.site.urls),
     #url('indecs', indecs, name='indecs'),
